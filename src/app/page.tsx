eb432b736d5dd21defbc62d1a6062d046c95a4fa
Aswin <me@aswin.sh>
2024-09-29 20:51:53 +0200

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,8 +51,8 @@ export default function Home() {
 
   return (
     <div className="relative min-h-screen">
-      <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h1 className="text-2xl font-bold">Figma Reactions</h1>
+      <div className="font-[family-name:var(--font-geist-sans)] text-center pt-20">
+        <h1 className="text-2xl font-bold select-none">Figma Reactions</h1>
         <div className="relative z-10">
           <PieShape
             ref={pieShapeRef}
@@ -61,7 +61,7 @@ export default function Home() {
             onEmojiSelect={handleEmojiSelect} // Add this line
           />
         </div>
-        <p className="text-sm text-gray-500 relative z-10">
+        <p className="text-sm text-gray-500 relative z-10 select-none">
           Press 'E' to show reactions, click outside to hide
         </p>
       </div>
